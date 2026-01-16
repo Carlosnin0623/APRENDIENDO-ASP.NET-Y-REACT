39173223474e93144bdcce899ff1035ee6f07c8e
Carlos Alberto Nin Queliz <128722564+Carlosnin0623@users.noreply.github.com>
2026-01-15 21:41:52 -0400

    Practica

---
+++ PeliculasApi/PeliculasApi/EjemploTiempoDeVidaDeLosServicios.cs
@@ -0,0 +1,15 @@
+﻿namespace PeliculasApi
+{
+    public class ServicioTransient
+    {
+        private readonly Guid _id;
+
+        public ServicioTransient()
+        {
+            _id = Guid.NewGuid();
+        }
+
+        public Guid ObtenerId => _id;
+
+    }
+}
